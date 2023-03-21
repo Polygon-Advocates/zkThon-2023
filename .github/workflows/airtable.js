7ad3d0b2259abba5ba1ef0a8b7f36c65a7022b30
PriyathamVarma <priyatham002@gmail.com>
2023-03-21 19:44:21 +0000

    check

--- .github/workflows/airtable.js
+++ .github/workflows/airtable.js
@@ -1,17 +1,23 @@
-const Airtable = require('airtable');
-const github = require('@actions/github');
+const Airtable = require("airtable");
+const github = require("@actions/github");
 
-const apiKey = 'keyJS9rxH1oNYtmGN';
-const baseId = 'appo8njU31iE50K5b';
-const base = new Airtable({apiKey: apiKey}).base(baseId);
+const apiKey = "keyJS9rxH1oNYtmGN";
+const baseId = "appo8njU31iE50K5b";
+const base = new Airtable({ apiKey: apiKey }).base(baseId);
+
+const prNumber = github.context.payload.pull_request.number;
+const prTitle = github.context.payload.pull_request.title;
+const prUrl = github.context.payload.pull_request.html_url;
+
+airtable.postPRToAirtable(prNumber, prTitle, prUrl);
 
 async function postPRToAirtable(prNumber, prTitle, prUrl) {
-  await base('Pull Requests').create({
-    'Number': prNumber,
-    'Title': prTitle,
-    'URL': prUrl
+  await base("Pull Requests").create({
+    Number: prNumber,
+    Title: prTitle,
+    URL: prUrl,
   });
-  console.log('Successfully posted PR to Airtable');
+  console.log("Successfully posted PR to Airtable");
 }
 
 exports.postPRToAirtable = postPRToAirtable;
